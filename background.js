3ec8fc6c5ddb221445c3f634cc8804c78c34d54f
Giovanne Bohms <giovannebohms@gmail.com>
2025-03-17 12:21:59 -0300

    feat: injeção js por link

--- background.js
+++ background.js
@@ -1,38 +1,6 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: 'OFF',
-  });
-});
-
-const url = 'https://proximal.unicarioca.edu.br/portal/egresso/';
-
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(url)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-
-    if (nextState === 'ON') {
-      await chrome.scripting
-        .executeScript({
-          target: { tabId: tab.id },
-          files: [
-            './src/main.js',
-           
-          ],
-        })
-        .then(() => console.log('script injected'));
-    } else if (nextState === 'OFF') {
-      await chrome.scripting
-        .executeScript({
-          target: { tabId: tab.id },
-          files: ['./off/off.js'],
-        })
-        .then(() => console.log('script injected'));
-    }
-  }
-});
+chrome.webNavigation.onCompleted.addListener((details) => {
+  chrome.scripting.executeScript({
+    target: { tabId: details.tabId },
+    files: ["src/main.js"]
+  }).then(() => console.log("✅ Script injetado automaticamente!"));
+}, { url: [{ hostContains: "proximal.unicarioca.edu.br" }] });
